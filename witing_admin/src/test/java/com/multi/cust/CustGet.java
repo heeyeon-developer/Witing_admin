0486e697b3e74d9552424dec6d464b17f05a0e0f
danbi0228 <qoeksql97@gmail.com>
2022-11-15 14:38:43 +0900

    danbi - cust

---
+++ witing_admin/src/test/java/com/multi/cust/CustGet.java
@@ -0,0 +1,29 @@
+package com.multi.cust;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.multi.dto.CustDTO;
+import com.multi.service.CustService;
+
+@SpringBootTest
+class CustGet {
+
+	@Autowired
+	CustService service;
+	
+	@Test
+	void contextLoads() {
+		try {
+			CustDTO cust = service.get("dbb");
+			System.out.println(cust);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
